07cbb05f4bd4a0ac70a6030dd43b33a15ca6b52a
Griffin <gmd298@gmail.com>
2022-03-17 11:52:29 -0400

    commit tree 1cfd13ee2f5eb6cb59677168eb7122eee104805e

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(namesArray) {
+    const thankYou = [];
+    for (let i = 0; i < namesArray.length; i++) {
+        thankYou.push(`Thank you, ${namesArray[i]}, for the wonderful surprise gift!`)
+    }
+    return thankYou
+}
+
+function countDown() {
+    for (let countDown = 10; countDown > -1; countDown--) {
+        console.log(countDown);
+    }
+}
